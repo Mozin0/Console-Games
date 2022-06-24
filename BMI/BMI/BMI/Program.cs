90fab26882bebb8d5019ec62c5251eac3425b155
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2022-06-24 22:03:18 +0200

    Add project files.

---
+++ BMI/BMI/BMI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int weight;
+            int height;
+            int age;
+            char m;
+            char f;
+            int BMI;
+
+            Console.WriteLine("Caluclate Your BMI");
+            Console.WriteLine("Enter your Details");
+            Console.Write("Weight (Kg):");
+            if (int.TryParse(Console.ReadLine(), out weight))
+            {
+                if (weight > 44 )
+                {
+                    
+                }
+
+            }
+            Console.Write("Height (cm) :");
+            if (int.TryParse(Console.ReadLine(), out height))
+            {
+
+            }
+            //Console.Write("Age:");
+            //do
+            //{
+
+            //    if (int.TryParse(Console.ReadLine(), out age))
+            //    {
+            //        if (age < 10)
+            //        {
+            //            Console.WriteLine("LOL");
+            //            break;
+            //        }
+            //    }
+            //    Console.Write("Gender (M or F):");
+            //    Console.ReadLine();         
+
+            //} while (true);
+        }
+    }
+}
---
+++ GuessNumberGame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessNumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1-100");
+            Random random = new();
+            var guessNumber = random.Next(1,100);
+            Console.WriteLine(guessNumber);
+
+            int result;
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out result) || result is < 1 or > 100)
+                {
+                    Console.WriteLine("Pick a valid number between 1-100");
+                    continue;
+                }
+                if (Math.Abs(guessNumber - result) <= 5)
+                {
+                    Console.WriteLine("You are close"); 
+                    if (result == guessNumber)
+                    {
+                        Console.WriteLine("You have guessed the Correct Number, Well Done!");
+                    }
+                }
+                
+                else if (result > guessNumber)
+                {
+                        Console.WriteLine("Thats too high");
+                }
+                else if (result < guessNumber)
+                {
+                        Console.WriteLine("Thats too low");
+                }               
+            } while (result != guessNumber);            
+        }
+    }
+}
