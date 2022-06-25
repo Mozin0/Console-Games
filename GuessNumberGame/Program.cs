0139fb6e6598f3af792bc948ff0bf81a0ccbf939
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2022-06-25 02:05:46 +0200

    Fix bugs

--- GuessNumberGame/Program.cs
+++ GuessNumberGame/Program.cs
@@ -6,37 +6,52 @@ namespace GuessNumberGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess a number between 1-100");
-            Random random = new();
-            var guessNumber = random.Next(1,100);
-            Console.WriteLine(guessNumber);
-
-            int result;
-            do
+            while (true)
             {
-                if (!int.TryParse(Console.ReadLine(), out result) || result is < 1 or > 100)
+
+
+                Console.WriteLine("Guess a number between 1-100");
+                Random random = new();
+                var guessNumber = random.Next(1, 100);
+
+                int result;
+
+                do
                 {
-                    Console.WriteLine("Pick a valid number between 1-100");
-                    continue;
-                }
-                if (Math.Abs(guessNumber - result) <= 5)
-                {
-                    Console.WriteLine("You are close"); 
+                    if (!int.TryParse(Console.ReadLine(), out result) || result is < 1 or > 100)
+                    {
+                        Console.WriteLine("Pick a valid number between 1-100");
+                        continue;
+                    }
                     if (result == guessNumber)
                     {
                         Console.WriteLine("You have guessed the Correct Number, Well Done!");
                     }
-                }
-                
-                else if (result > guessNumber)
-                {
+                    else if (Math.Abs(guessNumber - result) <= 5)
+                    {
+                        Console.WriteLine("You are close");
+                    }
+
+                    else if (result > guessNumber)
+                    {
                         Console.WriteLine("Thats too high");
-                }
-                else if (result < guessNumber)
-                {
+                    }
+                    else if (result < guessNumber)
+                    {
                         Console.WriteLine("Thats too low");
-                }               
-            } while (result != guessNumber);            
+                    }
+                    
+                } while (result != guessNumber);
+
+                Console.WriteLine("Press any key to Play again...");
+                Console.WriteLine("Press Escape if you want to quit playing");
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                {
+                    return;
+                }
+                Console.ReadKey(true);
+                Console.Clear();
+            }
         }
     }
 }
